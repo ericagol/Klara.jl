a5e969ba77213a4e40086f9ae7a5bcabc12abcd6
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2017-04-01 11:51:03 +0100

    Added main constructors for Gibbs factor

--- src/factors/GibbsFactor.jl
+++ src/factors/GibbsFactor.jl
@@ -1,5 +1,41 @@
 type GibbsFactor <: Factor
   cliques::Vector{Vector{Symbol}}
   potentials::Vector{Function}
-  keys::Vector{Symbol}
+  vertices::Vector{Symbol}
+  types::Vector{Symbol}
+  ofvertex::Dict{Symbol, Integer}
+end
+
+GibbsFactor(
+  cliques::Vector{Vector{Symbol}},
+  potentials::Vector{Function},
+  vertices::Vector{Symbol},
+  types::Vector{Symbol},
+  n::Integer=length(vertices)
+) =
+  GibbsFactor(cliques, potentials, vertices, types, Dict(zip(vertices, 1:n)))
+
+GibbsFactor(cliques::Vector{Vector{Symbol}}, potentials::Vector{Function}, vertices::Vector{Symbol}) =
+  GibbsFactor(cliques, potentials, vertices, Symbol[])
+
+GibbsFactor(cliques::Vector{Vector{Symbol}}, potentials::Vector{Function}) =
+  GibbsFactor(cliques, potentials, unique(vcat(cliques...)))
+
+function GibbsFactor(
+  cliques::Vector{Vector{Symbol}},
+  potentials::Vector{Function},
+  types::Dict{Symbol, Symbol},
+  n::Integer=length(types)
+)
+  local vkeys::Vector{Symbol} = Array(Symbol, n)
+  local vvals::Vector{Symbol} = Array(Symbol, n)
+  local i::Int64 = 1
+
+  for (k, v) in types
+    vkeys[i] = k
+    vvals[i] = v
+    i += 1
+  end
+
+  GibbsFactor(cliques, potentials, vkeys, vvals, n)
 end
