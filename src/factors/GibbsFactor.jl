c0fd46323bdd35e4cce945cfa0431d51f3639662
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2017-04-10 06:53:51 +0100

    WIP: redefining constructors of GibbsFactor

--- src/factors/GibbsFactor.jl
+++ src/factors/GibbsFactor.jl
@@ -3,43 +3,83 @@ type GibbsFactor <: Factor
   logpotentials::FunctionVector
   assignments::Vector{Pair{Symbol, Vector{Symbol}}}
   transforms::FunctionVector
-  vertices::Vector{Symbol}
-  vertextypes::Vector{DataType}
+  variables::Vector{Symbol}
+  variabletypes::Vector{DataType}
   support::RealPairVector
   reparametrize::Vector{Symbol} # :none or :log
-  ofvertex::Dict{Symbol, Integer}
+  ofvariable::Dict{Symbol, Integer}
 end
 
 num_cliques(f::GibbsFactor) = length(f.cliques)
 
 num_transforms(f::GibbsFactor) = length(f.assignments)
 
-num_vertices(f::GibbsFactor) = length(f.vertices)
+num_vertices(f::GibbsFactor) = length(f.variables)
 
 GibbsFactor(
   cliques::Vector{Vector{Symbol}},
   logpotentials::FunctionVector,
-  vertices::Vector{Symbol},
-  vertextypes::Vector{DataType},
+  assignments::Vector{Pair{Symbol, Vector{Symbol}}},
+  transforms::FunctionVector,
+  variables::Vector{Symbol},
+  variabletypes::Vector{DataType},
   support::RealPairVector,
   reparametrize::Vector{Symbol},
-  n::Integer=length(vertices)
+  n::Integer=length(variables)
 ) =
-  GibbsFactor(cliques, logpotentials, vertices, vertextypes, support, reparametrize, Dict(zip(vertices, 1:n)))
+  GibbsFactor(
+    cliques,
+    logpotentials,
+    assignments,
+    transforms,
+    variables,
+    variabletypes,
+    support,
+    reparametrize,
+    Dict(zip(variables, 1:n))
+  )
 
-GibbsFactor(cliques::Vector{Vector{Symbol}}, logpotentials::FunctionVector, vertices::Vector{Symbol}) =
-  GibbsFactor(cliques, logpotentials, vertices, DataType[], RealPair[], Symbol[])
-
-GibbsFactor(cliques::Vector{Vector{Symbol}}, logpotentials::FunctionVector) =
-  GibbsFactor(cliques, logpotentials, unique(vcat(cliques...)))
+GibbsFactor(
+  cliques::Vector{Vector{Symbol}},
+  logpotentials::FunctionVector,
+  variables::Vector{Symbol},
+  assignments::Vector{Pair{Symbol, Vector{Symbol}}}=Pair{Symbol, Vector{Symbol}}[],
+  transforms::FunctionVector=Function[],
+  n::Integer=length(variables)
+) =
+  GibbsFactor(cliques, logpotentials, assignments, transforms, variables, DataType[], RealPair[], Symbol[], n)
 
 function GibbsFactor(
   cliques::Vector{Vector{Symbol}},
   logpotentials::FunctionVector,
-  vertextypes::Dict{Symbol, DataType},
+  assignments::Vector{Pair{Symbol, Vector{Symbol}}}=Pair{Symbol, Vector{Symbol}}[],
+  transforms::FunctionVector=Function[]
+)
+  variables = isempty(assignments) ? unique(vcat(cliques...)) : unique(vcat(cliques..., [a.first for a in assignments]))
+
+  GibbsFactor(
+    cliques,
+    logpotentials,
+    assignments,
+    transforms,
+    variables,
+    DataType[],
+    RealPair[],
+    Symbol[],
+    length(variables)
+  )
+end
+
+# Defined constructors that take only variables as their input
+# To do: constructors for i) joint input of variables and variabletypes, ii) input of variabletypes only
+
+function GibbsFactor(
+  cliques::Vector{Vector{Symbol}},
+  logpotentials::FunctionVector,
+  variabletypes::Dict{Symbol, DataType},
   support::Dict{Symbol, RealPair},
   reparametrize::Dict{Symbol, Symbol},
-  n::Integer=length(vertextypes)
+  n::Integer=length(variabletypes)
 )
   local vkeys::Vector{Symbol} = Array(Symbol, n)
   local vvalues::Vector{DataType} = Array(DataType, n)
@@ -47,7 +87,7 @@ function GibbsFactor(
   local vtransform::Vector{Symbol} = Array(Symbol, n)
   local i::Int64 = 1
 
-  for (k, v) in vertextypes
+  for (k, v) in variabletypes
     vkeys[i] = k
     vvalues[i] = v
     vsupport[i] = get(support, k, Pair(-Inf, Inf))
@@ -61,15 +101,15 @@ end
 GibbsFactor(
   cliques::Vector{Vector{Symbol}},
   logpotentials::FunctionVector,
-  vertextypes::Dict;
+  variabletypes::Dict;
   support::Dict=Dict{Symbol, RealPair}(),
   reparametrize::Dict=Dict{Symbol, Symbol}(),
-  n::Integer=length(vertextypes)
+  n::Integer=length(variabletypes)
 ) =
   GibbsFactor(
     cliques,
     logpotentials,
-    convert(Dict{Symbol, DataType}, vertextypes),
+    convert(Dict{Symbol, DataType}, variabletypes),
     convert(Dict{Symbol, RealPair}, support),
     convert(Dict{Symbol, Symbol}, reparametrize),
     n
@@ -80,14 +120,14 @@ function codegen_logtarget(f::GibbsFactor, i::Integer, nc::Integer=num_cliques(f
   local lpargs::Vector
 
   for j in 1:nc
-    if in(f.vertices[i], f.cliques[j])
+    if in(f.variables[i], f.cliques[j])
       lpargs = []
 
       for v in f.cliques[j]
-        if v == f.vertices[i]
+        if v == f.variables[i]
           push!(lpargs, :(_state.value))
         else
-          push!(lpargs, :(_states[$(f.ofvertex[v])].value))
+          push!(lpargs, :(_states[$(f.ofvariable[v])].value))
         end
       end
 
@@ -98,7 +138,7 @@ function codegen_logtarget(f::GibbsFactor, i::Integer, nc::Integer=num_cliques(f
   @gensym logtarget
 
   quote
-    function $logtarget(_state::$(default_state_type(f.vertextypes[i])), _states::VariableStateVector)
+    function $logtarget(_state::$(default_state_type(f.variabletypes[i])), _states::VariableStateVector)
       $(body...)
     end
   end
@@ -108,10 +148,10 @@ function generate_variables(f::GibbsFactor, nc::Integer=num_cliques(f), nv::Inte
   local variables::VariableVector = Array(Variable, nv)
 
   for i in 1:nv
-    if issubtype(f.vertextypes[i], Parameter)
-      variables[i] = f.vertextypes[i](f.vertices[i], signature=:low, logtarget=eval(codegen_logtarget(f, i, nc)))
+    if issubtype(f.variabletypes[i], Parameter)
+      variables[i] = f.variabletypes[i](f.variables[i], signature=:low, logtarget=eval(codegen_logtarget(f, i, nc)))
     else
-      variables[i] = f.vertextypes[i](f.vertices[i])
+      variables[i] = f.variabletypes[i](f.variables[i])
     end
   end
 
